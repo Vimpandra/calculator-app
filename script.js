1a151c3403c4dbeebd95667f0a268ed4066421d5
Vimpandra <victorimperatriz@gmail.com>
2022-08-07 06:03:24 -0300

    Added functionality to all operations, point and equals

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ const n9 = document.getElementById(`n9`);
 const n0 = document.getElementById(`n0`);
 let numbers = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n0];
 
+const point = document.getElementById(`point`);
+
 const ac = document.getElementById(`ac`);
 const backspace = document.getElementById(`backspace`);
 
-let displayScreen = ``
+const addBtn = document.getElementById(`add`);
+const subtractBtn = document.getElementById(`subtract`);
+const multiplyBtn = document.getElementById(`multiply`);
+const divideBtn = document.getElementById(`divide`);
+const percentBtn = document.getElementById(`percent`);
+const equalsBtn = document.getElementById(`equals`);
+
+let displayScreen = ``;
+let firstValue = ``;
+let operation = ``;
 
 numbers.forEach(number => {
     number.addEventListener(`click`, () => {
@@ -24,8 +35,21 @@ numbers.forEach(number => {
     });
 });
 
+point.addEventListener(`click`, () => {
+    if (displayScreen.includes(`.`)) return;
+    else if (displayScreen === ``) {
+        displayScreen = `0.`;
+        screen.textContent = displayScreen;
+    } else {
+        displayScreen += point.value;
+        screen.textContent = displayScreen;
+    }
+});
+
 ac.addEventListener(`click`, () => {
-    displayScreen = ``
+    displayScreen = ``;
+    firstValue = ``;
+    operation = ``;
     screen.textContent = displayScreen;
 });
 
@@ -34,3 +58,77 @@ backspace.addEventListener(`click`, () => {
     screen.textContent = displayScreen;
 })
 
+addBtn.addEventListener(`click`, () => {
+    executeOperation();
+    operation = `add`
+});
+
+subtractBtn.addEventListener(`click`, () => {
+    executeOperation();
+    operation = `subtract`;
+});
+
+multiplyBtn.addEventListener(`click`, () => {
+    executeOperation();
+    operation = `multiply`;
+});
+
+divideBtn.addEventListener(`click`, () => {
+    executeOperation();
+    operation = `divide`;
+});
+
+percentBtn.addEventListener(`click`, () => {
+    executeOperation();
+    operation = `percent`;
+});
+
+equalsBtn.addEventListener(`click`, () => {
+    if (firstValue !== ``) {
+        operate();
+    }
+});
+
+// Basic Functions
+
+function operate() {
+    let result;
+    if (operation === `add`) {
+        result = add(Number(firstValue), Number(displayScreen));
+    } else if (operation === `subtract`) {
+        result = subtract(Number(firstValue), Number(displayScreen));
+    } else if (operation === `multiply`) {
+        result = multiply(Number(firstValue), Number(displayScreen));
+    } else if (operation === `divide`) {
+        result = divide(Number(firstValue), Number(displayScreen));
+    } else if (operation === `percent`) {
+        result = percent(Number(firstValue), Number(displayScreen));
+    }
+    firstValue = ``;
+    displayScreen = `${result}`;
+    screen.textContent = displayScreen;
+};
+
+function executeOperation() {
+    if (firstValue !== ``) {
+        operate();
+    }
+    firstValue = displayScreen;
+    displayScreen = ``;
+}
+
+function add(a, b) {
+    return a + b;
+};
+function subtract(a, b) {
+    return a - b;
+};
+function multiply(a, b) {
+    return a * b;
+};
+function divide(a, b) {
+    return a / b;
+};
+function percent(a, b) {
+    return (b / 100) * a;
+}
