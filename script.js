69f82791225d14af9d3b63212ce973e947506dbe
Vimpandra <victorimperatriz@gmail.com>
2022-08-09 18:12:47 -0300

    Added keyboard shortcuts with full functionality

--- script.js
+++ script.js
@@ -24,41 +24,27 @@ const divideBtn = document.getElementById(`divide`);
 const percentBtn = document.getElementById(`percent`);
 const equalsBtn = document.getElementById(`equals`);
 
-let displayScreen = ``;
+let screenContent = ``;
 let firstValue = ``;
 let operation = ``;
 
 numbers.forEach(number => {
     number.addEventListener(`click`, () => {
-        if (displayScreen.length >= 30) return;
-        else {
-            displayScreen += number.value;
-            updateScreen(displayScreen);
-        }
+        printNumber(number.value);        
     });
 });
 
 point.addEventListener(`click`, () => {
-    if (displayScreen.includes(`.`)) return;
-    else if (displayScreen === ``) {
-        displayScreen = `0.`;
-        screen.textContent = displayScreen;
-    } else {
-        displayScreen += point.value;
-        screen.textContent = displayScreen;
-    }
+    printPoint();
 });
 
 ac.addEventListener(`click`, () => {
-    displayScreen = ``;
-    firstValue = ``;
-    operation = ``;
-    screen.textContent = displayScreen;
+    clearAll();    
 });
 
 backspace.addEventListener(`click`, () => {
-    displayScreen = displayScreen.substr(0, displayScreen.length - 1);
-    updateScreen(displayScreen);
+    screenContent = screenContent.substr(0, screenContent.length - 1);
+    updateScreen(screenContent);
 })
 
 addBtn.addEventListener(`click`, () => {
@@ -95,41 +81,98 @@ equalsBtn.addEventListener(`click`, () => {
 // Keyboard Shortcuts
 
 window.addEventListener(`keydown`, (e) => {
-    if (e.key === `1`) {
-        if (displayScreen.length >= 30) return;
-        else {
-            displayScreen += number.value;
-            updateScreen(displayScreen);
+    if (e.key >= 0 && e.key <= 9) {
+        printNumber(e.key);
+    }
+    if (e.key === `+`) {
+        executeOperation();
+        operation = `add`;
+    }
+    if (e.key === `-`) {
+        executeOperation();
+        operation = `subtract`;
+    }
+    if (e.key === `*`) {
+        executeOperation();
+        operation = `multiply`;
+    }
+    if (e.key === `/`) {
+        executeOperation();
+        operation = `divide`;
+    }
+    if (e.key === `%`) {
+        executeOperation();
+        operation = `percent`;
+    }
+    if (e.key === `Enter` || e.key === `=`) {
+        if (firstValue !== ``) {
+            operate();
         }
     }
-})
+    if (e.key === `.`) {
+        printPoint();
+    }
+    if (e.key === `Backspace`) {
+        screenContent = screenContent.substr(0, screenContent.length - 1);
+        updateScreen(screenContent);
+    }
+    if (e.key === `Escape` || e.key === `Delete`) {
+        clearAll();
+    }
+});
 
 // Basic Functions
+
+function printNumber(n) {
+    if (screenContent.length >= 30) return;
+        else {
+            screenContent += n;
+            updateScreen(screenContent);
+        }
+}
+
+function printPoint() {
+    if (screenContent.includes(`.`)) return;
+    else if (screenContent === ``) {
+        screenContent = `0.`;
+        screen.textContent = screenContent;
+    } else {
+        screenContent += point.value;
+        screen.textContent = screenContent;
+    }
+}
+
+function clearAll() {
+    screenContent = ``;
+    firstValue = ``;
+    operation = ``;
+    screen.textContent = screenContent;
+}
 
 function operate() {
     let result;
     if (operation === `add`) {
-        result = add(Number(firstValue), Number(displayScreen));
+        result = add(Number(firstValue), Number(screenContent));
     } else if (operation === `subtract`) {
-        result = subtract(Number(firstValue), Number(displayScreen));
+        result = subtract(Number(firstValue), Number(screenContent));
     } else if (operation === `multiply`) {
-        result = multiply(Number(firstValue), Number(displayScreen));
+        result = multiply(Number(firstValue), Number(screenContent));
     } else if (operation === `divide`) {
-        result = divide(Number(firstValue), Number(displayScreen));
+        result = divide(Number(firstValue), Number(screenContent));
     } else if (operation === `percent`) {
-        result = percent(Number(firstValue), Number(displayScreen));
+        result = percent(Number(firstValue), Number(screenContent));
     }
     firstValue = ``;
-    displayScreen = `${result}`;
-    updateScreen(displayScreen);
+    screenContent = `${result}`;
+    updateScreen(screenContent);
 };
 
 function executeOperation() {
     if (firstValue !== ``) {
         operate();
     }
-    firstValue = displayScreen;
-    displayScreen = ``;
+    firstValue = screenContent;
+    screenContent = ``;
 }
 
 function add(a, b) {
